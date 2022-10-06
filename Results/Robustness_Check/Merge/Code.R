169b24e0a6d114bcfe5c1a05df7cdf96e60a9159
Keitaro-Ninomiya <67201847+Keitaro-Ninomiya@users.noreply.github.com>
2022-10-06 15:19:43 -0500

    Aggregate n=1 as seperate group

--- Results/Robustness_Check/Merge/Code.R
+++ Results/Robustness_Check/Merge/Code.R
@@ -8,7 +8,10 @@ Merge=function(dtaAgents,dtaAgents_Past){
     distinct(registration_no,estate_agent_license_no,registration_start_date,.keep_all=TRUE) %>%
     mutate(registration_start_date=floor_date(ymd(registration_start_date),"months"))
   Agency_List = dtaAgents %>% group_by(estate_agent_license_no,estate_agent_name) %>%
-    count() %>% as_tibble() %>% select(estate_agent_license_no) %>% as.list()
+    count() %>% as_tibble() %>%
+    mutate(estate_agent_license_no=case_when(n==1~"Small",
+                                             TRUE~estate_agent_license_no)) %>%
+    select(estate_agent_license_no) %>% distinct()
 
   Dates=seq(ymd("2021/01/01"),ymd("2022/06/01"),by="month")
   dta = expand.grid(Agency_List[[1]],y=Dates, KEEP.OUT.ATTRS = FALSE) %>% as_tibble() %>%
